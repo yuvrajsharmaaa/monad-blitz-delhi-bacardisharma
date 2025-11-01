a767e5b7a771ea485d3347ae3854d448cf2dd927
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 17:18:34 +0530

    feat: add hot update files for app layout and webpack
    - Introduced new hot update JSON and JS files for app layout and webpack.
    - Updated globals.css handling in the hot update process.
    - Implemented eval-source-map for better debugging during development.
    - Ensured compatibility with module hot acceptance for dynamic updates.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "808cc28df0056b03"; }
+/******/ 		__webpack_require__.h = function() { return "b3e17b926dc3581d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
