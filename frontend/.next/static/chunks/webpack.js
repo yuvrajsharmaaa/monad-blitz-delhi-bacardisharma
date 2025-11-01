8982dc984888fa281eb3fc9a74506e2b27d909d8
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 18:16:21 +0530

    Remove TypeScript type definitions for layout and page components in the frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "973eb7b582aeb615"; }
+/******/ 		__webpack_require__.h = function() { return "957f70fd4caa846c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
