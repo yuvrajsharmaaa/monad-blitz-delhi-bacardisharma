21c2c7208c561d9747bf51d87f1b27d3363fe744
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 15:39:08 +0530

    feat: update webpack hot module replacement files and layout styles
    - Added new hot-update JSON and JS files for app/layout with updated hash values.
    - Included changes to globals.css with new hash exports in multiple hot-update files.
    - Updated webpack runtime modules to reflect new hash values for better caching and performance.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b8176bd3fe6e8f6"; }
+/******/ 		__webpack_require__.h = function() { return "e3381a37227cc370"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
