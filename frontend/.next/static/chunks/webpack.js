5a1e2ba565e2df438d50d43f3f161cb4b3c7b448
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 17:59:56 +0530

    Add hot update files for layout and webpack with eval-source-map devtool
    - Created hot update JavaScript files for app layout and webpack.
    - Included globals.css in the layout hot update.
    - Added webpack runtime module for full hash retrieval.
    - Note: eval-source-map devtool is used for development purposes only.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d51667184767dc8e"; }
+/******/ 		__webpack_require__.h = function() { return "4255ee21b450d3a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
