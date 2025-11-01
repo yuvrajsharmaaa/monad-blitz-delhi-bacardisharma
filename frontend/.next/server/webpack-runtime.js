da442869bdd01a7f4650e538626653f274873b1d
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 19:47:22 +0530

    Add hot update files for layout and webpack with eval-source-map configuration
    - Created hot-update JSON for layout and not-found page.
    - Added hot-update JS for app layout and globals CSS with source maps.
    - Included hot-update JS for webpack runtime modules with full hash function.

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("66b50bc0c293f03e")
+/******/ 		__webpack_require__.h = () => ("75ea31798048faa9")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5df7420b90e60f5d"; }
+/******/ 		__webpack_require__.h = function() { return "254363f3418045fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
