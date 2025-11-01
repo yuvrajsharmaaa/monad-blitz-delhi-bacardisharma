73a7999727e5e6f4538fafc84642098634fc738c
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-01 17:47:05 +0530

    Initial Monad remix MVP

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66e6700ea151ca34"; }
+/******/ 		__webpack_require__.h = function() { return "70f2739d5570a33c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
